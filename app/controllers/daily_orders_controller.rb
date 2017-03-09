b0acdae5ae5b5c2a35b43fc7763ebea60cf9a53c
Payal Bhalerao <payal@joshsoftware.com>
2017-03-09 19:21:28 +0530

    [#56] Refactor DailyOrder controller index

--- app/controllers/daily_orders_controller.rb
+++ app/controllers/daily_orders_controller.rb
@@ -1,8 +1,5 @@
 class DailyOrdersController < ApplicationController
   def index
-    @daily_orders = OrderDetail.
-      joins(:order, :terminal).
-      group(:menu_item_name, "terminals.name, terminals.id").
-      select("sum(quantity) as total_quantity, menu_item_name, terminals.name as terminal_name, terminals.id as t_id")
+    @daily_orders = OrderDetail.daily_orders
   end  
 end
