87144a1f3f0b91e1e9dc2f351837cdaf566646a1
tejaswini-gambhire <gambhire.tejaswini30@gmail.com>
2017-03-09 21:40:25 +0530

    Changes routes, removes dependency of users on company

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   root to: 'home#index'
 
   devise_for :users
-  resources :companies do
-    resources :users
-  end
+  resources :companies
+  resources :users
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :daily_orders, only: [:index]
 end
